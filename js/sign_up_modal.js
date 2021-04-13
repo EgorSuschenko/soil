e147f01c306bc190ffcbfa3bac9b55957f44e129
Sergey <my_name_sprite@mail.ru>
2021-04-13 16:36:38 +0300

    fixing styling

--- js/sign_up_modal.js
+++ js/sign_up_modal.js
@@ -9,7 +9,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
